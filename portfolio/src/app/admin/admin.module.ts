476d4d8dd4f43ccd20962f32f08722f17471657f
Carolina <crodriguez@iesrafaelalberti.es>
2024-05-14 21:25:19 +0200

    flex-end

--- portfolio/src/app/admin/admin.module.ts
+++ portfolio/src/app/admin/admin.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { SharedModule } from '../shared/shared.module';
 
